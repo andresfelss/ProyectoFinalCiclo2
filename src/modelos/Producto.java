3f2e80dee903618616aeded65cd0838cc376ce66
andre <andres.felipepr17@gmial.com>
2022-07-22 11:35:14 -0500

    Modelo  producto creado

--- src/modelos/Producto.java
+++ src/modelos/Producto.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelos;
 
-/**
- *
- * @author andre
- */
+
 public class Producto {
+    
+    private long codProducto;
+    private String nombre;
+    private String id;
+    private double temperatura;
+    private double valorBase;
+
+    public Producto() {
+    }
+
+    public long getCodProducto() {
+        return codProducto;
+    }
+
+    public void setCodProducto(long codProducto) {
+        this.codProducto = codProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getValorBase() {
+        return valorBase;
+    }
+
+    public void setValorBase(double valorBase) {
+        this.valorBase = valorBase;
+    }
+    
     
 }
