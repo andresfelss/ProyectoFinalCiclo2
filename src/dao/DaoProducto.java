caf971b046e543841b8dec2b473eea16ab402005
andre <andres.felipepr17@gmial.com>
2022-07-25 11:53:33 -0500

    Modelo  producto creado

--- src/dao/DaoProducto.java
+++ src/dao/DaoProducto.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
-/**
- *
- * @author andre
- */
-public class DaoProducto {
+import configuracion.Conexion;
+import interfaces.Operaciones;
+import java.util.List;
+import modelos.Producto;
+
+public class DaoProducto extends Conexion implements Operaciones<Producto> {
+
+    @Override
+    public boolean agregar(Producto objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public List<Producto> consultar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean borrar(Producto objeto) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public Producto buscar(long llavePK) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
     
 }
